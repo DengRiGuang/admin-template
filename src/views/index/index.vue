662746dd68f2aa2b12d0c6dc66698237c0e2e43b
dengriguang <dengriguang@hnpmct.com>
2021-03-10 14:20:09 +0800

    fix:测试请求

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: dengriguang@hnpmct.com
  * @since: 2021-02-25 10:44:23
- * @lastTime: 2021-03-10 11:47:22
+ * @lastTime: 2021-03-10 14:17:51
  * @LastAuthor: Do not edit
  * @文件相对于项目的路径: \admin-template\src\views\index\index.vue
  * @Description: 
